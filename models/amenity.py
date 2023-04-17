e9d7f99a7c7549b7c7fa76e36cc619161af676dd
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-18 02:36:50 +0300

    models/amenity.py updated 1

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+A module for defining Amenity class that inherits
+from BaseModel and stores data in a SQL database.
+"""
+
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    A class for representing amenities in the HBNB project.
+
+    Attributes:
+    -----------
+    name : str
+        The name of the amenity.
+    """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False) if storage_type == 'db' else ''
