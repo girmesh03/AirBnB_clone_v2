e541d911807f95bc4ec8479659dbd2008d51ff09
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-18 02:40:25 +0300

    models/city.py updated 1

--- models/city.py
+++ models/city.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""
+City Module for HBNB project
+Defines the City class that inherits from BaseModel
+"""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """
+    Represents a city.
+
+    Attributes:
+        state_id (str): the ID of the state where the city is located
+        name (str): the name of the city
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Instantiates a new City object.
+
+        Args:
+            args: Variable length argument list.
+            kwargs: Arbitrary keyword arguments.
+        """
+        super().__init__(*args, **kwargs)
+        self.state_id = kwargs.get("state_id", "")
+        self.name = kwargs.get("name", "")
