8e66affc5b9d6c240ab5d20033753dfb73c1b211
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-04 20:04:24 +0300

    100-clean_web_static.py added 1

---
+++ 100-clean_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 3-deploy_web_static.py)
+that deletes out-of-date archives, using the function do_clean:
+"""
+
+from fabric.api import env, local, run, cd, lcd
+
+env.hosts = ['100.25.167.139', '54.172.232.30']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+
+def do_clean(number=0):
+    """
+    Deletes all unnecessary archives in the versions and releases folders
+    """
+    number = int(number)
+    if number < 1:
+        number = 1
+    with lcd("versions"):
+        local("ls -t | tail -n +{} | xargs rm -f".format(number + 1))
+    with cd("/data/web_static/releases"):
+        run("ls -t | tail -n +{} | xargs rm -rf".format(number + 1))
