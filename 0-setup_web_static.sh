42cab3d3120783fbc24d5eedf3a1ef9151616ca3
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-11 07:33:51 +0300

    0-setup_web_static.sh new added 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,33 +21,8 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 
-printf %s "server {
-
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    add_header X-Served-By $HOSTNAME;
-
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 https://www.facebook.com;
-    }
-
-    error_page 404 /404.html;
-
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-
-}" > /etc/nginx/sites-available/default
+web_static="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm index.nginx-debian.html;\n\t}"
+sed -i "/server_name _;/a\\$web_static" /etc/nginx/sites-available/default
 
 # Restart nginx
 service nginx restart
