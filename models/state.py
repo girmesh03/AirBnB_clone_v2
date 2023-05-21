c157673821836abb56a1135c0f255f7403ed0424
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-21 22:18:28 +0300

    state.py is modified

--- models/state.py
+++ models/state.py
@@ -3,32 +3,25 @@
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
 
 
 class State(BaseModel, Base):
     """State class inherits from BaseModel and Base"""
-    __tablename__ = 'states'
-    name = Column(
-        String(128),
-        nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         @property
         def cities(self):
-            """Returns the cities in this State instance"""
-            from models import storage
-            cities_in_state = []
-            for value in storage.all(City).values():
-                if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+            """getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
+
+            cities = models.storage.all(City).values()
+            cities_list = [city for city in cities if self.id == city.state_id]
+            return cities_list
